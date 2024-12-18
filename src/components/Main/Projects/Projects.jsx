6797afac092bedd629e05b62a9a2a0cdde25ab56
Deydre <deydrealonso@gmail.com>
2024-12-18 16:46:52 +0100

    add/first hero approach

--- src/components/Main/Projects/Projects.jsx
+++ src/components/Main/Projects/Projects.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return <>
+    <section>
+    Projects
+    </section>
+  </>;
 };
 
 export default Projects;
