44a892221180bb290aecc142466b1b919d858529
Deydre <deydrealonso@gmail.com>
2025-01-09 11:29:33 +0100

    add/footer responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,11 +6,11 @@ const Footer = () => {
     <p>Deydre Alonso Rosillo</p>
     <section>
       <article>
-        <a href="">Contacto</a>
+        <a href="mailto:deydrealonso@gmail.com">Contacto</a>
       </article>
       <article>
         <a href="https://github.com/Deydre" target="_blank"><FaGithub className="footerIcon" /></a>
-        <a href="https://www.linkedin.com/in/deydre/" target="_blank"><FaLinkedin className="footerIcon" /></a>  
+        <a href="https://www.linkedin.com/in/deydre/" target="_blank"><FaLinkedin className="footerIcon" /></a>
       </article>
     </section>
   </footer>;
